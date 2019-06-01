bee75c18c5c4bba691488ed8712f673f843f2847
MikevPeeren <MikevPeeren@hotmail.com>
2019-06-01 18:01:36 +0200

    Refactor to use Express 💨

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+// Core Node Modules
 const http = require('http');
 
-const routes = require('./routes');
+// Third Party Node Modules
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(1337);
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/add-product', (request, response, next) => {
+    response.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>')
+});
+
+app.get('/product', (request, response, next) => {
+    console.log(request.body);
+    response.redirect('/');
+});
+
+app.use('/', (request, response, next) => {
+    console.log('In another Middleware!');
+    response.send('<h1>Hello from Express</h1>');
+});
+
+app.listen(1337);
